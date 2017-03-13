3b834ce8d36e246ca540b94d32698937ef59d5d7
Preto95 <sebastian-giese@posteo.de>
2017-03-13 08:26:51 +0100

    Renamed standard form name

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace SiebzehnundVier
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmSuV());
         }
     }
 }
--- frmSuV.cs
+++ frmSuV.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SiebzehnundVier
 {
-    public partial class Form1 : Form
+    public partial class frmSuV : Form
     {
-        public Form1()
+        public frmSuV()
         {
             InitializeComponent();
         }
