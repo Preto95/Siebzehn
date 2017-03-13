229d453cf3716d002e5f853d4d0f9bc3ce84a70c
Preto95 <sebastian-giese@posteo.de>
2017-03-13 09:16:17 +0100

    Card added, Class Diagram added
    abstract class Card and a class diagram added.

---
+++ Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SiebzehnundVier
+{
+    abstract class Card
+    {
+        protected Image cardImage;   //object for the Card-View
+
+        public Card(Image cardImage)
+        {
+            this.cardImage = cardImage;
+        }
+        
+        public Image CardImage
+        {
+            private set { cardImage = value; }
+            get { return cardImage; }
+        }
+    }
+}
