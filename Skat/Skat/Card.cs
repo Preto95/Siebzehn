54767f75d90db296b04f4e1108c034027b7a9da6
Preto95 <sebastian-giese@posteo.de>
2017-03-15 09:43:52 +0100

    Deckcomparison

--- Skat/Skat/Card.cs
+++ Skat/Skat/Card.cs
@@ -22,8 +22,9 @@ namespace SiebzehnundVier
         /// <summary>
         /// Randomize the objects of a list
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
+        /// <typeparam name="T">Datatype</typeparam>
+        /// <param name="list">A list of objects</param>
+        /// <returns>Returns a radomized list</returns>
         public static List<T> Shuffle<T>(List<T> list)
         {
             Random rand = new Random();
@@ -45,15 +46,104 @@ namespace SiebzehnundVier
         /// <summary>
         /// Count the Points
         /// </summary>
-        /// <param name="deck0"></param>
-        /// <returns></returns>
-        public static int[] CompareDecks(params Card[] deck0)
+        /// <param name="deck0">Deck of Player 1</param>
+        /// <param name="deck1">Deck of Player 2</param>
+        /// <returns>Returns a sorted list of points per deck</returns>
+        public static List<int> CompareDecks(List<Card> deck0, List<Card> deck1)
         {
-            int[] pointsofDecks = new int[4] { 0, 0, 0, 0 };
+            List<int> pointsofDecks = new List<int>(4);
 
             #region Counting Points
-
+            foreach (Card card in deck0)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            foreach (Card card in deck1)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
             #endregion
+
+            pointsofDecks.Sort();
+
+            return pointsofDecks;
+        }
+
+        /// <summary>
+        /// Count the Points
+        /// </summary>
+        /// <param name="deck0">Deck of Player 1</param>
+        /// <param name="deck1">Deck of Player 2</param>
+        /// <param name="deck2">Deck of Player 3</param>
+        /// <returns>Returns a sorted list of points per deck</returns>
+        public static List<int> CompareDecks(List<Card> deck0, List<Card> deck1, List<Card> deck2)
+        {
+            List<int> pointsofDecks = new List<int>(4);
+
+            #region Counting Points
+            foreach (Card card in deck0)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            foreach (Card card in deck1)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            foreach (Card card in deck2)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            #endregion
+
+            pointsofDecks.Sort();
+
+            return pointsofDecks;
+        }
+
+        /// <summary>
+        /// Count the Points
+        /// </summary>
+        /// <param name="deck0">Deck of Player 1</param>
+        /// <param name="deck1">Deck of Player 2</param>
+        /// <param name="deck2">Deck of Player 3</param>
+        /// <param name="deck3">Deck of Player 4</param>
+        /// <returns>Returns a sorted list of points per deck</returns>
+        public static List<int> CompareDecks(List<Card> deck0, List<Card> deck1, List<Card> deck2, List<Card> deck3)
+        {
+            List<int> pointsofDecks = new List<int>(4);
+
+            #region Counting Points
+            foreach (Card card in deck0)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            foreach (Card card in deck1)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            foreach (Card card in deck2)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            foreach (Card card in deck3)
+            {
+                pointsofDecks.Add((int)card.PointS);
+            }
+            #endregion
+
+            pointsofDecks.Sort();
+
+            return pointsofDecks;
+        }
+
+        //public void deinemudda(params Card[] tmp)
+        //{
+        //    tmp[0].lst.
+        //}
+
+        public static List<Card> GenerateDecks(int playerCount)
+        {
 
         }
     }
+}
